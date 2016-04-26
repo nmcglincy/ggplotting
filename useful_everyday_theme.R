bd0a72ce1746a214e8d11280a2b1d2a938fd633f
Nicholas McGlincy <nmcglincy@gmail.com>
2016-04-25 17:51:15 -0700

    adding some...
    … more advice

--- useful_everyday_theme.R
+++ useful_everyday_theme.R
@@ -3,3 +3,4 @@ theme(strip.background=element_rect(fill = NA, colour = "black"),
 	strip.text.y = element_text(angle = 0),
 	panel.border = element_rect(fill = NA, colour = "black"))
 # the golden ratio is 1.618
+# remember to add a \n before your x, and after your y, axis label for proper spacing
