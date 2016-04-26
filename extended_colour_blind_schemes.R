ef80609b114621cfc74c36e95e4a6a4f54eabbd8
Nicholas McGlincy <nmcglincy@gmail.com>
2016-04-25 17:28:07 -0700

    added...
    …some other useful things

---
+++ extended_colour_blind_schemes.R
@@ -0,0 +1,5 @@
+# The palette with grey:
+cbPalette <- c("#999999", "white", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# The palette with black:
+cbbPalette <- c("#000000", "white", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
---
+++ useful_everyday_theme.R
@@ -0,0 +1,5 @@
+theme_bw() +
+theme(strip.background=element_rect(fill = NA, colour = "black"),
+	strip.text.y = element_text(angle = 0),
+	panel.border = element_rect(fill = NA, colour = "black"))
+# the golden ratio is 1.618
